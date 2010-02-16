a6fcd8e0dbbd9b65422d552badb8a18a0870c56c
pornelski@gmail.com <pornelski@gmail.com@7e1ae596-858b-11dd-ba2e-09f2ff3e973f>
2010-02-16 23:59:32 +0000

    Refactored queueing to avoid race condition in check for end of queue
    git-svn-id: http://imageoptim.googlecode.com/svn/trunk@58 7e1ae596-858b-11dd-ba2e-09f2ff3e973f

--- imageoptim/ImageOptim.m
+++ imageoptim/ImageOptim.m
@@ -51,7 +51,7 @@
 // invoked by Dock
 - (BOOL)application:(NSApplication *)sender openFile:(NSString *)path
 {
-    [filesQueue addPath:path dirs:[filesQueue extensions]];
+    [filesQueue addPath:path];
 	[filesQueue runAdded];
 	return YES;
 }
